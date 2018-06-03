9afddf6600fe9b17a7a31c04fed35ed3cc62f2d1
Zeydi <oussema.zeidi@ieee.org>
2018-06-03 12:49:33 +0100

    fixed port for heroku deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express();
+const PORT = process.env.PORT || 3000;
 
 var path = require('path');
 
@@ -8,6 +9,6 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
-app.listen(3000,() => {
-  console.log('app is running at port 3000');
+app.listen(PORT,() => {
+  console.log(`app is running at port ${PORT}` );
 })
